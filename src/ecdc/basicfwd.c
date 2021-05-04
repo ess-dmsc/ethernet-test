35bfa8ecbb0ecc4022ba26407dcd02c097241a92
Morten Jagd Christensen <mortenjc@jcaps.com>
2021-05-04 10:11:24 +0200

    added statistics counter structs and printstats (not integrated yet)

--- src/ecdc/basicfwd.c
+++ src/ecdc/basicfwd.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright(c) 2010-2015 Intel Corporation
  */
+// ECDC
 
 #include <stdint.h>
 #include <inttypes.h>
@@ -22,6 +23,61 @@ static const struct rte_eth_conf port_conf_default = {
 		.max_rx_pkt_len = RTE_ETHER_MAX_LEN,
 	},
 };
+
+/* Per-port statistics struct */
+struct l2fwd_port_statistics {
+	uint64_t rx_pkt;
+	uint64_t rx_bytes;
+	uint64_t dropped;
+} __rte_cache_aligned;
+struct l2fwd_port_statistics port_statistics[RTE_MAX_ETHPORTS];
+
+static void
+print_stats(void)
+{
+	uint64_t total_packets_dropped, total_packets_rx, total_bytes_rx;
+	unsigned portid;
+
+	total_packets_dropped = 0;
+	total_packets_rx = 0;
+	total_bytes_rx = 0;
+
+	const char clr[] = { 27, '[', '2', 'J', '\0' };
+	const char topLeft[] = { 27, '[', '1', ';', '1', 'H','\0' };
+
+		/* Clear screen and move to top left */
+	printf("%s%s", clr, topLeft);
+
+	printf("\nPort statistics ====================================");
+
+	for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++) {
+		/* skip disabled ports */
+		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
+			continue;
+		printf("\nStatistics for port %u ------------------------------"
+			   "\nPackets received: %24"PRIu64
+			   "\nBytes received  : %25"PRIu64
+			   "\nPackets dropped : %21"PRIu64,
+			   portid,
+			   port_statistics[portid].rx_pkt,
+				 port_statistics[portid].rx_bytes,
+			   port_statistics[portid].dropped);
+
+		total_packets_dropped += port_statistics[portid].dropped;
+		total_packets_rx += port_statistics[portid].rx_pkt;
+		total_bytes_rx += port_statistics[portid].rx_bytes;
+	}
+	printf("\nAggregate statistics ==============================="
+		   "\nTotal packets received: %18"PRIu64
+		   "\nTotal bytes received: %19"PRIu64
+		   "\nTotal packets dropped: %15"PRIu64,
+		   total_packets_rx_pkt,
+		   total_packets_rx_bytes,
+		   total_packets_dropped);
+	printf("\n====================================================\n");
+
+	fflush(stdout);
+}
 
 /* basicfwd.c: Basic DPDK skeleton forwarding example. */
 
