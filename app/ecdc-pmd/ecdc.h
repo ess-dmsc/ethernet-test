c6aa83f1603407b316a1e0d7a317494861cf7992
Morten Jagd Christensen <morten.christensen@ess.eu>
2021-05-11 17:10:20 +0200

    trimmed includes

--- app/ecdc-pmd/ecdc.h
+++ app/ecdc-pmd/ecdc.h
@@ -1,7 +1,7 @@
 
 
 
-
+///
 struct ess_hdr {
   uint8_t Padding;
   uint8_t Version;
@@ -17,10 +17,14 @@ struct ess_hdr {
 } __attribute__((packed));
 
 
+///
 int ecdc_eth_counters(struct rte_mbuf  * pkt, struct fwd_stream *fs);
 
+///
 int ecdc_ip_counters(struct rte_mbuf  * pkt, struct fwd_stream *fs);
 
+///
 int ecdc_essdaq_counters(struct rte_mbuf  * pkt, struct fwd_stream *fs);
 
+///
 void ecdc_rx_packet(struct rte_mbuf  * pkt, struct fwd_stream *fs);
--- app/ecdc-pmd/rxonly.c
+++ app/ecdc-pmd/rxonly.c
@@ -2,41 +2,10 @@
  * Copyright(c) 2010-2014 Intel Corporation
  */
 
-#include <stdarg.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <inttypes.h>
-
-#include <sys/queue.h>
-#include <sys/stat.h>
-
 #include <rte_common.h>
-#include <rte_byteorder.h>
-#include <rte_log.h>
-#include <rte_debug.h>
-#include <rte_cycles.h>
-#include <rte_memory.h>
-#include <rte_memcpy.h>
-#include <rte_launch.h>
-#include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_lcore.h>
-#include <rte_atomic.h>
-#include <rte_branch_prediction.h>
-#include <rte_mempool.h>
 #include <rte_mbuf.h>
-#include <rte_interrupts.h>
-#include <rte_pci.h>
 #include <rte_ether.h>
 #include <rte_ethdev.h>
-#include <rte_string_fns.h>
-#include <rte_ip.h>
-#include <rte_udp.h>
-#include <rte_net.h>
-#include <rte_flow.h>
 
 #include "testpmd.h"
 #include "ecdc.h"
